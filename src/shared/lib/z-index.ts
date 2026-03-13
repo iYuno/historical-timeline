012b683fc78f6fc234782fcaa95ca16635447154
Логинов Роман Александрович <rloginov_tk@inno.local>
2026-03-13 23:05:46 +0400

    feat: add axios client and z-index utility with exports

---
+++ src/shared/lib/z-index.ts
@@ -0,0 +1,23 @@
+/** biome-ignore-all lint/style/useLiteralEnumMembers: <explanation> */
+const BASE_INDEX = 100;
+
+function getZIndex(level: number) {
+	return BASE_INDEX - (9 - level);
+}
+
+export enum ZIndexKind {
+	notification = getZIndex(9),
+	tooltip = getZIndex(8),
+	hint = getZIndex(8),
+	dropdown = getZIndex(6),
+	confirmation = getZIndex(6),
+	modal = getZIndex(5),
+	drawer = getZIndex(4),
+	backdrop = getZIndex(2),
+	header = getZIndex(1),
+
+	/** для позиционирования в рамках родительского блока */
+	relative = 1,
+	/** для позиционирования над всем контентом. для исключительных случаев */
+	absolute = getZIndex(10),
+}
